3e68b4ae2cee7e8e836b1b743b91c2e59d91336f
jerry <1091231787@qq.com>
2017-06-16 10:36:05 +0800

    first commit

---
+++ resources/assets/js/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="panel-heading">Example Component</div>
+
+            <div class="panel-body">
+                hello,Jerry!
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted2.')
+        }
+    }
+</script>
